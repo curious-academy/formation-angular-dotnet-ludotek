4ef0e0c2e5e4c6c9775d08f6979d92438a570e91
Evan BOISSONNOT <evan@boissonnot.fr>
2023-11-24 23:37:07 +0100

    feat: tools to get custom preload strategy

--- front-end/my-ludotek-ws/projects/front-end/src/app/app.routes.ts
+++ front-end/my-ludotek-ws/projects/front-end/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'books',
-    loadChildren: () => import('book-features').then(item => item.booksRoutes)
+    loadChildren: () => import('book-features').then(item => item.booksRoutes),
+    data: {preload: true}
   }
 ];
