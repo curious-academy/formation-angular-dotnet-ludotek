6f0e63668b4957838b145c986fcebd12af172ae5
Evan BOISSONNOT <evan@boissonnot.fr>
2023-11-24 22:55:00 +0100

    core: prepare lib for book context

---
+++ front-end/my-ludotek-ws/projects/book-features/src/lib/book-features.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-book-features',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      book-features works!
+    </p>
+  `,
+  styles: ``
+})
+export class BookFeaturesComponent {
+
+}
