127f6345ac8a1aeb04e52cef7a0501ce2096e064
Evan BOISSONNOT <evan@boissonnot.fr>
2023-12-28 18:16:59 +0100

    poc: add fake filter

--- front-end/my-ludotek-ws/projects/book-features/src/lib/services/books.infrastructure.ts
+++ front-end/my-ludotek-ws/projects/book-features/src/lib/services/books.infrastructure.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable, delay, of } from "rxjs";
 import { Book, Books } from "../models";
+import { bookTable } from "./fakes";
 
 export interface BookFilter {
   value: string;
@@ -12,15 +13,8 @@ export interface GetAllBooks {
 
 export const inMemoryGetAllBooks: GetAllBooks = {
   getAll: (filter: BookFilter) => {
-    const table: Books = [{
-      id: 1,
-      title: 'Dune',
-      author: {
-        name: 'Frank Herbert'
-      }
-    }];
-
-    return of(table).pipe(delay(1500));
+    const filterTable = bookTable.filter(item => item.title.toLowerCase().startsWith(filter.value.toLowerCase()))
+    return of(bookTable).pipe(delay(1500));
   }
 }
 
---
+++ front-end/my-ludotek-ws/projects/book-features/src/lib/services/fakes/index.ts
@@ -0,0 +1,25 @@
+import { Books } from "../../models";
+
+export const bookTable: Books = [
+  {
+    id: 1,
+    title: 'Dune',
+    author: {
+      name: 'Frank Herbert'
+    }
+  },
+  {
+    id: 2,
+    title: 'La stratégie Ender',
+    author: {
+      name: 'Orson scott card'
+    }
+  },
+  {
+    id: 3,
+    title: 'Le cycle des inhibiteurs',
+    author: {
+      name: 'Arthur Reynolds'
+    }
+  },
+];
